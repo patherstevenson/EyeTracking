7500ced2e164bf90727a06366f88fde40caa9651
pather <stevenson.pather.etu@univ-lille.fr>
2025-11-02 20:56:59 +0100

    separate Calib Dataset

--- src/tracker/Calibration.py
+++ src/tracker/Calibration.py
@@ -20,53 +20,7 @@ from torch.utils.data import Dataset
 
 from utils.utils import pixels_to_gaze_cm, get_numbered_calibration_points, euclidan_distance_radius
 from utils.config import SCREEN_WIDTH, SCREEN_HEIGHT, CALIBRATION_IMAGE_PATH, CALIBRATION_PTS
-
-
-class CalibrationDataset(Dataset):
-    """
-    PyTorch Dataset for storing calibration data.
-    This dataset is used to fine-tune the gaze tracking model with user-specific gaze samples.
-    """
-
-    def __init__(self, calibration_data: list[tuple[tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor], tuple[float, float]]]):
-        """
-        Initializes the dataset.
-
-        :param calibration_data: List of tuples (features, gaze_targets).
-                                 Each entry contains extracted features and the corresponding gaze target.
-        :type calibration_data: list[tuple[tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor], tuple[float, float]]]
-        """
-        self.data = calibration_data
-
-    def __len__(self) -> int:
-        """
-        Returns the number of samples in the dataset.
-
-        :return: Number of calibration samples.
-        :rtype: int
-        """
-        return len(self.data)
-
-    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        """
-        Retrieves a sample from the dataset.
-
-        :param idx: Index of the sample.
-        :type idx: int
-        :return: Tuple (faces, eyes_left, eyes_right, face_grids, gaze_targets).
-        :rtype: tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]
-        """
-        features, gaze_targets = self.data[idx]
-        face_input, left_eye_input, right_eye_input, face_grid_input = features
-
-        return (
-            face_input.squeeze(0),  # Remove batch dimension
-            left_eye_input.squeeze(0),
-            right_eye_input.squeeze(0),
-            face_grid_input.squeeze(0),
-            torch.tensor(gaze_targets, dtype=torch.float32)
-        )
-
+from tracker.CalibrationDataset import CalibrationDataset
 
 class Calibration:
     """
--- src/tracker/__init__.py
+++ src/tracker/__init__.py
@@ -1,6 +1,7 @@
 from .GazeModel import GazeModel
+from .CalibrationDataset import CalibrationDataset
 from .GazeTracker import GazeTracker
 from .GazeDataLogger import GazeDataLogger
 from .Calibration import Calibration
 
-__all__ = ["GazeModel", "GazeTracker", "GazeDataLogger", "Calibration"]
+__all__ = ["GazeModel", "CalibrationDataset", "GazeTracker", "GazeDataLogger", "Calibration",]
