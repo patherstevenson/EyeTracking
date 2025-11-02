0ec343a5169e7738b1f852c0aa54fa95d1a12788
pather <stevenson.pather.etu@univ-lille.fr>
2025-11-02 20:23:06 +0100

    model_path in name

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ def main() -> None:
         print("Unable to open webcam. Please check your device or URL.")
         return
 
-    gaze_tracker = GazeTracker()
+    gaze_tracker = GazeTracker(model_path="itracker_baseline.tar")
 
     try:
         gaze_tracker.run(webcam)
